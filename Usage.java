955f0f0b3139f0a59fb825958bb0980435cf33d3
ndangnguyen <31484530+ndangnguyen@users.noreply.github.com>
2019-04-02 11:20:40 +0700

    Create Usage.java

---
+++ Usage.java
@@ -0,0 +1,5 @@
+Activity
+cUtils.setFont(context, (ViewGroup) ((ViewGroup)findViewById(android.R.id.content)).getChildAt(0));
+
+Popup
+cUtils.setFont(context, (ViewGroup) ((ViewGroup)dialog.findViewById(android.R.id.content)).getChildAt(0));
